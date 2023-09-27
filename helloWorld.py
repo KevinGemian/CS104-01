1713c219eb4290d9494297e02b7a01b7fb59c011
KevinGemian <146225525+KevinGemian@users.noreply.github.com>
2023-09-27 12:38:58 -0400

    This is version 1 of my hello world program

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,5 @@
 Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> #this is Version 1 of my hello world program
+>>> #This is Version 1 of my hello world program
+>>> print ('Hello my name is Kevin')
+Hello my name is Kevin

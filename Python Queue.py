b77ce51219e3eeef812728cef3d3f9cf608d3e0b
KevinGemian <146225525+KevinGemian@users.noreply.github.com>
2023-12-04 22:59:58 -0500

    Python Queue

---
+++ Python Queue.py
@@ -0,0 +1,10 @@
+names = []
+i = 0
+for i in range (10):
+    name = input ("Enter a name: ")
+    names. append(name)
+print (names)
+i = 0
+for I in range(10):
+    len(names)
+    print(names.pop(0))
